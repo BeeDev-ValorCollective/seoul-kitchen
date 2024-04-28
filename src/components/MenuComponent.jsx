52968655f27380f6e0adb488048e814f1f743c7d
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-04-28 15:14:57 -0400

    saving

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -1,14 +1,38 @@
-
+import React, { useState, useEffect } from 'react'
+import Dakkalbi from '../static/assets/Dakkalbi.png'
+import Galbi from '../static/assets/Galbi.png'
+import Kimbap from '../static/assets/Kimbap.jpg'
+import OreoCroffle from '../static/assets/OreoCroffle.jpg'
+import StrawbCroffle from '../static/assets/StrawbCroffle.jpg'
 
 const MenuComponent = () => {
+    const [imgURL, setImgURL] = useState('')
 
+    const imgList = [
+        Dakkalbi,
+        Galbi,
+        Kimbap,
+        OreoCroffle,
+        StrawbCroffle
+        ]
+
+    const getRandomImg = () => {
+        const randIndex = Math.floor(Math.random() * imgList.length);
+        return imgList[randIndex];
+    };
+        
+    // Set random image URL on component mount or reload
+    useEffect(() => {
+        const randImg = getRandomImg();
+        setImgURL(randImg);
+    }, []);
 
     return (
         <>
             <div className="wrapper wrapper_border">
                 <div className="split_box">
-                    <div className="split_box_text">
-                        <p>Text</p>
+                    <div className="split_box_img">
+                        {imgURL && <img src={imgURL} alt="Food Image" />}
                     </div>
                     <div className="split_box_button">
                         <button>Button</button>
--- src/components/Slideshow_Component/Slideshow.jsx
+++ src/components/Slideshow_Component/Slideshow.jsx
@@ -3,7 +3,7 @@ const Slideshow = () => {
 
     return (
         <>
-        <div className="wrapper wrapper_border">
+        <div className="wrapper">
             <ul className="slideshow">
                 <li><span></span></li>
                 <li><span></span></li>
